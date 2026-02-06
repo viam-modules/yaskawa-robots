34e1178bc671775f0818c91edccf306c5580c4f6
Nicolas Menard <nicolas.p.menard@gmail.com>
2026-02-06 13:52:51 -0500

    correct test include

--- src/viam/lib/test/realtime_trajectory_logger_test.cpp
+++ src/viam/lib/test/realtime_trajectory_logger_test.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_MODULE RealtimeTrajectoryLoggerTest
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <json/json.h>
 #include <Eigen/Core>
