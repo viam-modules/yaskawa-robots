dbbb676dcd27ba06b41937f7b246687ada899259
Nicolas Menard <nicolas.p.menard@gmail.com>
2026-02-15 17:14:09 -0500

    Trajex (#38)
    * Add new trajectory generator
    
    * update configs
    
    * update dockerfile back to noble
    
    * small fixes
    
    * added segmentation config
    
    * format

--- src/viam/lib/robot_socket.hpp
+++ src/viam/lib/robot_socket.hpp
@@ -453,6 +453,10 @@ class YaskawaController : public std::enable_shared_from_this<YaskawaController>
     // Move locking: prevents concurrent moves
     std::atomic<bool> move_in_progress_{false};
 
+    bool use_new_trajectory_planner_{false};
+    double path_tolerance_rad_{0.1};
+    std::optional<double> collinearization_ratio_;
+    double segmentation_threshold_rad_;
     std::string robot_model_;
     std::optional<std::function<std::optional<std::string>()>> telemetry_path_fn_;
 
